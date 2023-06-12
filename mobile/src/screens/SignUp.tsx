13efe4c3a1fb1b06ea94d704dc4add23329cfb7d
haruoSugano <haruosugano@icloud.com>
2023-06-11 21:19:31 -0300

    feat: configurando a integracao com api e tratamento de erros

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,14 +1,19 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
-import { useNavigation } from "@react-navigation/native";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useForm, Controller } from "react-hook-form";
-import * as yup from "yup";
+import { useNavigation } from "@react-navigation/native";
 import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import axios from "axios";
+import { api } from "@services/api";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
     name: string;
@@ -29,15 +34,31 @@ export function SignUp() {
     const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         resolver: yupResolver(signUpSchema)
     });
-    
+
     const navigation = useNavigation();
+    const toast = useToast();
 
     function handleGoBack() {
         navigation.goBack();
     }
 
-    function handleSignUp(data: FormDataProps) {
-        console.log(data);
+    async function handleSignUp({ name, email, password, passwordConfirm }: FormDataProps) {
+
+        try {
+            const response = await api.post('/users', { name, email, password });
+
+            console.log(response.data);
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : "Não foi possível criar a conta. Tente novamente mais tarde";
+
+            toast.show({
+                title,
+                placement: "top",
+                bgColor: "red.500"
+            });
+        }
+
     }
 
     return (
