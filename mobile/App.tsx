8e784e63211447489f3b1db902e820253c83db21
haruoSugano <haruosugano@icloud.com>
2023-06-20 22:51:21 -0300

    feat: criando context e hook e compartilhando estado no contexto

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,9 +2,12 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Routes } from '@routes/index';
+
+import { AuthContextProvider } from '@contexts/AuthContext';
+
 import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
-import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,8 +19,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <AuthContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
